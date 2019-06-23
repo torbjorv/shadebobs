fbb1ab5bede4213c67b4658c43cdf585331b2da3
Torbjorn Vik <torbjornvik@torbjorns-air.lan>
2019-06-23 09:28:27 -0700

    us github pages instead for deployment. cheaper

--- bin/update.sh
+++ bin/update.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
-ng build --prod
-gcloud app deploy . --quiet
+ng build --prod --base-href "https://torbjorv.github.io/shadebobs/"
+npx angular-cli-ghpages
+
+# ng build --prod
+# gcloud app deploy . --quiet
  
