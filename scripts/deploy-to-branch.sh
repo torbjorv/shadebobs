a5a1e8b612dfef54cace7e7df093f7e09318c715
CircleCI <torbjorv@gmail.com>
2019-07-10 10:14:43 -0700

    see if we can get the staging vs prod thign working

---
+++ scripts/deploy-to-branch.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+TOKEN=$1
+BRANCH=$2
+BRANCH_DIR=branch-$BRANCH
+
+# All git output below is sent to dev/null to avoid exposing anything sensitive in build logs
+
+echo checkout $BRANCH_DIR
+mkdir $BRANCH_DIR
+cd $BRANCH_DIR
+
+git config --global user.name "CircleCI"  > /dev/null 2>&1
+git init  > /dev/null 2>&1
+git remote add --fetch origin https://$TOKEN@github.com/torbjorv/shadebobs.git > /dev/null 2>&1
+
+git checkout $BRANCH_DIR  > /dev/null 2>&1
+
+shopt -s extglob
+rm -rf . !("README.md") > /dev/null 2>&1
+
+# Copy angular app in here
+echo copy app
+cp -a "../dist/." .
+cp index.html 404.html
+
+echo add files
+git add -A > /dev/null 2>&1
+
+echo commit and push
+# need 'ci skip' to ignore this branch in CircleCI
+git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"  > /dev/null 2>&1
+git push --force --quiet origin $BRANCH_DIR > /dev/null 2>&1
+
+echo cleanup
+cd ..
+rm -rf $BRANCH_DIR
