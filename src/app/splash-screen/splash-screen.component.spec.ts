0affc55642b3e1e0a1865a4ce193feafb922e4d9
CircleCI <torbjorv@gmail.com>
2019-07-18 22:48:30 -0700

    fix test

--- src/app/splash-screen/splash-screen.component.spec.ts
+++ src/app/splash-screen/splash-screen.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SplashScreenComponent } from './splash-screen.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SplashScreenComponent', () => {
   let component: SplashScreenComponent;
@@ -8,6 +9,7 @@ describe('SplashScreenComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ NoopAnimationsModule ],
       declarations: [ SplashScreenComponent ]
     })
     .compileComponents();
