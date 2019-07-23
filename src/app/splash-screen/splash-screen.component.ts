b9513d4e2a6959b468d405d0f83829966b90d95a
CircleCI <torbjorv@gmail.com>
2019-07-22 19:17:45 -0700

    avoid duplicating the splashscreen divs

--- src/app/splash-screen/splash-screen.component.ts
+++ src/app/splash-screen/splash-screen.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, AfterViewInit, ChangeDetectorRef, Input } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ChangeDetectorRef, Input, Inject, ViewChild, ElementRef } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-splash-screen',
@@ -22,12 +23,20 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 export class SplashScreenComponent implements AfterViewInit {
 
   public fade = false;
-  public done = false;
 
-  constructor(private _changeDetector: ChangeDetectorRef) { }
+  @ViewChild('container', {static: false}) private _container: ElementRef<HTMLElement>;
+
+  constructor(
+    private _changeDetector: ChangeDetectorRef,
+    @Inject(DOCUMENT) private _document: HTMLDocument) { }
 
   ngAfterViewInit() {
     this.fade = true;
     this._changeDetector.detectChanges();
+
+    // Move the splashscreen element from index.html to here so we can apply the fade
+    const splashScreen: HTMLElement = this._document.getElementById('splashscreen');
+    splashScreen.parentElement.removeChild(splashScreen);
+    this._container.nativeElement.appendChild(splashScreen);
   }
 }
